ce54d39f381fe76d90e1872bb7fbda65f4dab5a2
Karim Mohamed <karimmohmdd01@gmail.com>
2025-11-28 21:21:23 +0200

    Add files via upload

--- Production/app.py
+++ Production/app.py
@@ -1,6 +1,8 @@
 import sys
 import os
-sys.path.append(os.path.abspath("src"))
+#sys.path.append(os.path.abspath("src"))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
+
 
 
 import streamlit as st
